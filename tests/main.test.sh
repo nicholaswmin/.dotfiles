09cbf810a622a03b5a59bb9f116955a9fcadcfb7
Nicholas Kyriakides <nik.kyriakides@gmail.com>
2025-06-09 19:44:51 +0000

    refactor: update test script for dotfiles manager to improve CI compatibility

--- tests/main.test.sh
+++ tests/main.test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env zsh
-echo "=== TESTING DOTFILES MANAGER ==="
+echo "=== TESTING IN CI ==="
+# Override the library path to current directory
+export DOTFILES_ROOT="$PWD"
+export DOTFILES_HOME="$PWD/home"
+
+./dotfiles --help
+./dotfiles --version
 ./dotfiles init
-./dotfiles link ~/.zshrc
-./dotfiles backup "test"
 echo "=== DONE ==="
