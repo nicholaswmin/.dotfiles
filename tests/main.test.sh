b77a796b8102d24d48085d83043a6867390f89f2
Nicholas Kyriakides <nik.kyriakides@gmail.com>
2025-06-09 19:42:20 +0000

    DSAS

--- tests/main.test.sh
+++ tests/main.test.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env zsh
-printf "PWD: %s\n" "$PWD"
-ls -la
-printf "\n"
-
-if [[ -f dotfiles ]]; then
-  printf "dotfiles exists\n"
-  if [[ -x dotfiles ]]; then
-    printf "dotfiles is executable\n"
-    ./dotfiles --help 2>&1 | head -5
-  else
-    printf "dotfiles not executable\n"
-    chmod +x dotfiles
-  fi
-else
-  printf "dotfiles missing\n"
-fi
-
-exit 0
+echo "=== TESTING DOTFILES MANAGER ==="
+./dotfiles init
+./dotfiles link ~/.zshrc
+./dotfiles backup "test"
+echo "=== DONE ==="
